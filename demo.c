9f953cecb84e4231be2933524f86f5211e5d348d
Matheus Fiebig <macrubig@gmail.com>
2022-05-15 17:59:27 -0400

    dev final se deus quiser amem

--- demo.c
+++ demo.c
@@ -7,12 +7,10 @@
 #include <sys/wait.h>
 #include <pthread.h>
 
-int returnValue = 0;
-int pipes = 0;
-int pipeIterator = 0;
-int* fds = NULL;
+static int isEqualTo(char* argv, char* token) {
+    if(token == "NULL" || argv == NULL || token == NULL)
+        return 1;
 
-int isOperand(char* argv, char* token) {
     if(token != NULL){
         return strcmp(argv, token) == 0;
     }
@@ -20,141 +18,73 @@ int isOperand(char* argv, char* token) {
 	return strcmp(argv, "&&") == 0 || strcmp(argv, "||") == 0 || strcmp(argv, "|") == 0;
 }
 
-char** createCopy(int argc, char** argv) {
-    char** newArray = (char**)malloc(sizeof(char*) * argc);
-    for (int i = 0; i < argc; i++) {
-        newArray[i] = (char*)malloc(sizeof(char) * strlen(argv[i]));
-    }
+static void pipeline(char ***cmd)
+{
+	int fd[2];
+	pid_t pid;
+	int fdd = 0;				/* Backup */
 
-    for (int i = 0; i < argc; i++) {
-        strcpy(newArray[i], argv[i]);
-    }
-
-    return newArray;
+	while (*cmd != NULL) {
+		pipe(fd);				/* Sharing bidiflow */
+		if ((pid = fork()) == -1) {
+			perror("fork");
+			exit(1);
+		}
+		else if (pid == 0) {
+			dup2(fdd, 0);
+			if (*(cmd + 1) != NULL) {
+				dup2(fd[1], 1);
+			}
+			close(fd[0]);
+			execvp((*cmd)[0], *cmd);
+			exit(1);
+		}
+		else {
+			wait(NULL); 		/* Collect childs */
+			close(fd[1]);
+			fdd = fd[0];
+			cmd++;
+		}
+	}
 }
 
-int getNextCommand(char** commands, int actualCommandIndex){
-    if(actualCommandIndex == -1)
-        return 0;
-
-    int numberOfParameters = 1;
-    int i = actualCommandIndex;
-    while (commands[i] != NULL){
-        numberOfParameters++;
-        i++;
-    }
-    return numberOfParameters + actualCommandIndex;
-}
-
-int isBackground(char** commands, int actualCommandIndex, int argc){
-    int i = actualCommandIndex;
-    while (commands[i] != NULL && i < argc - 1){
-        if(strcmp(commands[i], "&") == 0)
-            return 1;
-        i++;
-    }
-    return 0;
-}
-
-int countPipes(char** argv, int argc){
-    int cont = 0, i = 0;
-    while(argv[i] != NULL && i < argc - 1){
-        if(isOperand(argv[i], "|")) cont++;
-        i++;
-    }
-    return cont;
-}
-
-void openPipes(int numberOfPipes){
-    for(int i = 0; i < numberOfPipes; i++){
-        if(pipe(fds + i*2) == -1){
-            perror("Erro de pipe");
-            exit(EXIT_FAILURE);
-        } 
-    }
-}
-
-void closePipes(int numberOfPipes, int fds[], int factor){
-    for(int j = 0; j < 2*numberOfPipes-factor; j++){
-        if(close(fds[j]) == -1){
-            perror("Erro de close");
-            exit(EXIT_FAILURE);
+static void logic_operation(char *** cmd){
+    int returnValue = 0;
+    while (*cmd != NULL) {
+        pid_t pid = fork();
+		if (pid == 0) {
+                    printf("%s %d\n",(*cmd)[0], returnValue);
+        if(
+            (isEqualTo((*cmd)[0], "&&") && returnValue == 0)
+            ||(isEqualTo((*cmd)[0], "||") && returnValue != 0)
+        ){
+            exit(1);
+            return;
         }
-    }
+			execvp((*cmd)[0], *cmd);
+			exit(1);
+		} else {
+			waitpid(pid, &returnValue, 0);
+			cmd++;
+		}
+	}
 }
 
-void configurePipe(char* op, pid_t p){
-    if(pipes != 0){
-        if(pipeIterator != 0){
-            printf("Error: %d", pipeIterator);
-            if(dup2(fds[2*pipeIterator - 2], STDIN_FILENO) == -1){
-                perror("Erro de dup entrada");
-                exit(EXIT_FAILURE);
-            }
-        }
+int main(int argc, char *argv[])
+{
+	//char *c1[] = {"ls", "-la", NULL};
+	//char *c2[] = {"grep", "demo.c", NULL};
+	//char *c3[] = {"grep", "demo", NULL};
+	//char **cmd[] = {c1, c2, c3, NULL};
+	//pipeline(cmd);
 
-        if(pipeIterator*2 != 2*pipes){
-            if(dup2(fds[2*pipeIterator + 1], STDOUT_FILENO) == -1){
-                perror("Erro de dup saida");
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
+	char *c1[] = {"echo", "teste", NULL};
+    char *o1[] = {"&&"};
+	char *c2[] = {"echo", "teste2", NULL};
+    char *o2[] = {"||"};
+	char *c3[] = {"echo", "teste3", NULL};
+	char **cmd[] = {c1, o1 , c2, o2, c3, NULL};
+    logic_operation(cmd);
 
-    closePipes(pipes,fds,1);
+	return (0);
 }
-
-int countNumberOfCommands(char** commands, int argc){
-    int numeroDeComandos = 1;
-    for(int i = 0; i < argc-1; i++){
-        if(isOperand(commands[i],NULL) == 1 || isOperand(commands[i],"&") == 1){
-            if(isOperand(commands[i],"&") == 0) numeroDeComandos++;
-            commands[i] = NULL;
-        }
-    }
-    commands[argc - 1] = NULL;
-    return numeroDeComandos;
-}
-
-void execute(char** commands, int startIndex, char* operation){
-    pid_t p = fork();
-    if(p == 0){
-        configurePipe(operation,p);
-        execvp(commands[startIndex], &commands[startIndex]);
-    }
-    
-    pipeIterator++;
-}
-
-void executeOperand(char* op, char** commands, int nextCommandIndex){
-    if(strcmp(op, "&") == 0 || strcmp(op, "|") == 0){
-        execute(commands, nextCommandIndex, op);
-    }
-}
-
-int main(int argc, char** argv) {
-    char** copyArgv = createCopy(argc,argv);
-	char** commands = &argv[1];
-    
-    int numeroDeComandos = countNumberOfCommands(commands, argc);
-    int commandIndex = -1; 
-
-    pipes = countPipes(copyArgv, argc);
-    fds = (int*)malloc(sizeof(int)*pipes*2);
-    openPipes(pipes);
-
-    while(numeroDeComandos-- > 0){
-        commandIndex = getNextCommand(commands,commandIndex);
-        if(isOperand(copyArgv[commandIndex],NULL)){
-            executeOperand(copyArgv[commandIndex], commands, commandIndex);
-            continue;
-        }
-        execute(commands,commandIndex, "");
-    }
-
-    closePipes(pipes,fds,0);
-    for(int j = 0; j < pipes; j++) waitpid(0,NULL,0);
-	
-    return 0;
-}
-
--- shell.c
+++ shell.c
@@ -8,10 +8,16 @@
 #include <pthread.h>
 
 int returnValue = 0;
+int commandsNumber=0;
 char** commands = NULL;
 char** copyArgv = NULL;
+int pipes = 0;
+int nargs= 0;
 
-int isOperand(char* argv, char* token) {
+int is(char* argv, char* token) {
+    if(token == "NULL")
+        return 1;
+
     if(token != NULL){
         return strcmp(argv, token) == 0;
     }
@@ -35,6 +41,7 @@ char** createCopy(int argc, char** argv) {
 int getNextCommand(int actualCommandIndex){
     if(actualCommandIndex == -1)
         return 0;
+
     int numberOfParameters = 1;
     int i = actualCommandIndex;
     while (commands[i] != NULL){
@@ -44,21 +51,12 @@ int getNextCommand(int actualCommandIndex){
     return numberOfParameters + actualCommandIndex;
 }
 
-int isBackground(int actualCommandIndex, int argc){
-    int i = actualCommandIndex;
-    while (copyArgv[i] != NULL && i < argc - 1){
-        if(strcmp(copyArgv[i], "&") == 0)
-            return 1;
-        i++;
-    }
-    return 0;
-}
-
 int countNumberOfCommands(int argc){
     int numeroDeComandos = 1;
     for(int i = 0; i < argc-1; i++){
-        if(isOperand(commands[i],NULL) == 1 || isOperand(commands[i],"&") == 1){
-            if(!isOperand(commands[i],"&")) numeroDeComandos++;
+        if(is(commands[i],"&")) numeroDeComandos--;
+        if(is(commands[i],NULL) == 1 || is(commands[i],"&") == 1){
+            numeroDeComandos++;
             commands[i] = NULL;
         }
     }
@@ -66,40 +64,107 @@ int countNumberOfCommands(int argc){
     return numeroDeComandos;
 }
 
-void execute(int commandIndex, char* operator){   
+char*** chainPipes(int actual, int next, char* op){
+    int numberOfPipes = 0;
+    while(is(op,"|")){
+        numberOfPipes++;
+        next = getNextCommand(next);
+        op = next >= nargs ? "NULL": copyArgv[next];
+    }
+
+    char ***cmd = (char***) malloc(sizeof(char**)*numberOfPipes+2);
+    for(int i = 0; i < numberOfPipes+2; i++){
+        cmd[i] = (char**)malloc(sizeof(char*) * 20);
+        for(int j = 0; j < 20; j++){
+            cmd[i][j] = (char*) malloc(sizeof(char)*20);
+        }
+    }
+
+    int j = 0, p = 0;
+    for(p = 0; p < numberOfPipes+1; p++){
+        int i = actual;
+        while(commands[i] != NULL){
+            cmd[p][i] = commands[j];
+            i++;j++;
+        }
+        cmd[p][i] = NULL;
+        j++;
+    }
+
+    cmd[p] = NULL;
+    
+    return cmd;
+}
+
+void execute(int commandIndex, char* op){   
     pid_t p = fork();
     if(p == 0){
+        if(is(op,"&")) setpgid(0,0);
         execvp(commands[commandIndex], &commands[commandIndex]);
-    }else{
-        waitpid(p,&returnValue,0);
+    }else if(p > 0 && !is(op,"&")){
+        wait(&returnValue);
     }
 }
 
-void executeOperand(char* op, int actualCommandIndex, int nextCommandIndex){
-    if(
-        (isOperand(op,"&&") && returnValue == 0) || 
-        isOperand(op,"&")
-      )
-    {
-        execute(actualCommandIndex, op);
+void executePipe(char ***cmd)
+{
+	pid_t pid;
+	int fd[2];
+	int in = 0;
+	while (*cmd != NULL) {
+		pipe(fd);
+        pid = fork();
+		if (pid == -1) {
+			exit(1);
+		} else if (pid == 0) {
+			dup2(in, 0);
+			if (*(cmd + 1) != NULL) {
+				dup2(fd[1], 1);
+			}
+			close(fd[0]);
+			execvp((*cmd)[0], *cmd);
+			exit(1);
+		} else {
+			wait(&returnValue);
+			close(fd[1]);
+			in = fd[0];
+			cmd++;
+		}
+	}
+}
+
+void executeOperand(int actualCommandIndex, char* opBefore, char* opAfter, int nextCommandIndex){
+    if(is(opAfter,"|")){
+        char*** cmd = chainPipes(actualCommandIndex, nextCommandIndex, opAfter);
+        executePipe(cmd);
         return;
     }
 
-    execute(actualCommandIndex, op);
+    if(is(opAfter,"&")){
+        execute(actualCommandIndex,"&");
+    }
+
+    if( (is(opBefore,"||") && returnValue != 0) 
+        || (is(opBefore,"&&") && returnValue == 0)
+        || !is(opBefore, NULL)
+    ){
+        execute(actualCommandIndex,"");   
+    }
 }
 
 int main(int argc, char** argv) {
+    nargs = argc;
     copyArgv = createCopy(argc,argv);
 	commands = &argv[1];
-    
-    int numeroDeComandos = countNumberOfCommands(argc);
+    commandsNumber = countNumberOfCommands(argc);
     
     int commandIndex = -1, nextCommandIndex = -1; 
-    while(numeroDeComandos-- > 0){
+    while(commandsNumber-- > 0){
         commandIndex = getNextCommand(commandIndex);       
-        nextCommandIndex = getNextCommand(commandIndex);
-        int bg = isBackground(commandIndex, argc);
-        executeOperand(bg ? "&" : copyArgv[commandIndex], commandIndex, nextCommandIndex);
+        nextCommandIndex = getNextCommand(commandIndex);       
+        char* opBefore = copyArgv[commandIndex];
+        char* opAfter = nextCommandIndex >= argc ? "NULL": copyArgv[nextCommandIndex];
+        executeOperand(commandIndex, opBefore, opAfter, nextCommandIndex);
     }
 
 	return 0;
